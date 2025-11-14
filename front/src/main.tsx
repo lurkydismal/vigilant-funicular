2f725d110f1f46a2c5a27ecdb09127b5e705da45
lurkydismal <lurkydismal@duck.com>
2025-11-14 14:47:11 +0000

    1

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,12 +1,21 @@
+import './index.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
+import App from './App';
 import { StyledEngineProvider } from '@mui/material/styles';
-import App from './App.tsx';
-import './index.css';
+import { apiGateway, isDev } from './stdvar';
+import { enableInterceptors } from './interceptors';
+import { log } from './stdlog';
+
+enableInterceptors();
+
+log.info(`Running ${isDev ? 'DEVELOPMENT' : 'PRODUCTION'} build`);
+
+log.debug(`API gateway: '${apiGateway}'`);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
---
+++ front/src/stdvar.ts
@@ -0,0 +1,3 @@
+export const needTrace = false;
+export const isDev = import.meta.env.DEV;
+export const apiGateway = import.meta.env.VITE_API_GATEWAY ?? '/mockGate';
