debb9dc2e87422b4ddd1698d16ac1b9916e6fa09
lurkydismal <lurkydismal@duck.com>
2026-02-18 16:38:21 +0500

    1

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,6 +1,6 @@
 import Footer from "@/components/Footer";
 import NavBar from "@/components/NavBar";
-import { Container } from "@mui/material";
+import { Box, Container } from "@mui/material";
 
 export default function Layout({
     children,
@@ -8,17 +8,24 @@ export default function Layout({
     children: React.ReactNode;
 }>) {
     return (
-        <>
+        <Box
+            sx={{
+                minHeight: "100vh",
+                display: "flex",
+                flexDirection: "column",
+            }}
+        >
             <NavBar />
 
             <Container
                 component="main"
                 maxWidth="xl"
                 sx={{
+                    flexGrow: 1,
                     display: "flex",
                     flexDirection: "column",
-                    marginTop: 11,
-                    marginBottom: 4,
+                    mt: 11,
+                    mb: 4,
                     gap: 4,
                 }}
             >
@@ -26,6 +33,6 @@ export default function Layout({
             </Container>
 
             <Footer />
-        </>
+        </Box>
     );
 }
--- src/app/(protected)/new-post/page.tsx
+++ src/app/(protected)/new-post/page.tsx
@@ -4,24 +4,9 @@ import { Container } from "@mui/material";
 export default function NewPost() {
     return (
         <Container
-            component="main"
             maxWidth="xl"
             sx={{
-                // display: "flex",
-                // flexdirection: "column",
-                // margintop: 13,
-                // marginbottom: 4,
-                // gap: 4,
-
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center", // center horizontally
-                justifyContent: "flex-start",
-                mt: 24,
-                marginBottom: 5,
-                px: { xs: 2, sm: 3 },
-                py: { xs: 3, sm: 6 },
-                gap: 4,
+                pt: "6%",
             }}
         >
             <MainContent />
