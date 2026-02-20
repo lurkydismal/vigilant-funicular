f8e3d10ffdbe5bc1a71887edf9356ea19d0e1149
lurkydismal <lurkydismal@duck.com>
2026-02-20 14:39:23 +0500

    1

--- src/app/(protected)/posts/page.tsx
+++ src/app/(protected)/posts/page.tsx
@@ -1,20 +1,29 @@
 import { tags } from "@/data/posts";
+import { desc } from "drizzle-orm";
 import MainFallback from "@/components/MainFallback";
 import Latest from "@/components/posts/Latest";
 import MainContent from "@/components/posts/MainContent";
 import db from "@/db";
 import { postWithCategory } from "@/utils/validate/schemas";
+import { categories, posts } from "@/db/schema";
 
 export default async function Posts() {
-    const _posts = await db.query.posts.findMany({
-        orderBy: {
-            created_at: "desc",
-        },
+    // const _posts = await db.query.posts.findMany({
+    //     orderBy: {
+    //         created_at: "desc",
+    //     },
+    //
+    //     with: {
+    //         category: true,
+    //     },
+    // });
 
-        with: {
-            category: true,
-        },
-    });
+    const _posts = await db
+        .select()
+        .from(posts)
+        .leftJoin(categories, categories.id.equals(posts.category_id))
+        .orderBy(desc(posts.created_at))
+        .execute();
 
     const parsed = postWithCategory.array().parse(_posts);
 
