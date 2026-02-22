d3c84090d2b94f785c02a2e94d705db743fc210c
lurkydismal <lurkydismal@duck.com>
2026-02-22 10:47:08 +0000

    1

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,12 +1,19 @@
 import Footer from "@/components/Footer";
 import NavBar from "@/components/NavBar";
+import { getSessionData } from "@/lib/auth";
 import { Box, Container } from "@mui/material";
+import { redirect } from "next/navigation";
 
-export default function Layout({
-    children,
-}: Readonly<{
+export default async function ProtectedLayout({ children }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const user = await getSessionData();
+
+    if (!user) {
+        // redirect to login if not authenticated
+        redirect("/login");
+    }
+
     return (
         <Box
             sx={{
