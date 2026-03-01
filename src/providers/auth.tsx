fe61a87175c564897b77067ac5fd4c9a5f1e5af8
lurkydismal <lurkydismal@duck.com>
2026-03-01 09:29:55 +0000

    1

--- src/providers/auth.tsx
+++ src/providers/auth.tsx
@@ -12,7 +12,13 @@ type AuthContextType = {
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
-    const [user, setUser] = useState<UsersRowPublic | null>(null);
+    const [user, setUser] = useState<UsersRowPublic | null>(() => {
+        if (typeof window === "undefined") return null;
+
+        const stored = localStorage.getItem("user");
+
+        return stored ? JSON.parse(stored) : null;
+    });
     const [version, setVersion] = useState(0);
 
     const _setUser = (u: UsersRowPublic | null) => {
@@ -21,6 +27,9 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
         log.debug(`Passed user: ${u}`);
 
         setUser(u);
+
+        if (u) localStorage.setItem("user", JSON.stringify(u));
+        else localStorage.removeItem("user");
 
         setVersion(v => v + 1);
     };
