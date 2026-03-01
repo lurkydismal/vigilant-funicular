58d062fba2344cea5b90b0d971d7bab2a544385e
lurkydismal <lurkydismal@duck.com>
2026-03-01 13:39:24 +0000

    1

--- drizzle/20260301131244_yielding_jasper_sitwell/migration.sql
+++ drizzle/20260301131244_yielding_jasper_sitwell/migration.sql
@@ -1,14 +1,2 @@
-C-- 1. Rename old type
-ALTER TYPE post_visibility RENAME TO post_visibility_old;
-
--- 2. Create new type
-CREATE TYPE post_visibility AS ENUM ('public', 'hidden', 'private');
-
--- 3. Update all columns using it
-ALTER TABLE 'posts'
-ALTER COLUMN visibility
-TYPE post_visibility
-USING visibility::text::post_visibility;
-
--- 4. Drop old type
-DROP TYPE post_visibility_old;REATE TYPE "post_visibility" AS ENUM('public', 'unlisted', 'private');--> statement-breakpoint
+CREATE TYPE "post_visibility" AS ENUM('public', 'unlisted', 'private');--> statement-breakpoint
+ALTER TABLE "posts" ALTER COLUMN "visibility" SET DEFAULT 'public'::"post_visibility";
