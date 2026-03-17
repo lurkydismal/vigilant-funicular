6a51b4fb7e2338d47529d7f44ad7a64b7963ec7f
LurkyDismal <lurkydismal@duck.com>
2026-03-17 13:40:07 +0000

    Remove unused files

--- front/src/stdlog.ts
+++
@@ -1,8 +0,0 @@
-import { Logger } from 'tslog';
-import { isDev, needTrace } from './stdvar';
-
-export const log = new Logger({
-    type: 'pretty',
-    minLevel: needTrace ? 1 : isDev ? 2 : 3,
-    prettyLogTemplate: '{{logLevelName}}\t{{filePathWithLine}}\t{{msg}}',
-});
--- front/src/stdvar.ts
+++
@@ -1,3 +0,0 @@
-export const needTrace = false;
-export const isDev = import.meta.env.DEV;
-export const apiGateway = import.meta.env.VITE_API_GATEWAY ?? '/mockGate';
